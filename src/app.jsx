88a87bf731c2379ec00314a09c31163c63659e63
ajie123100 <2946478276@qq.com>
2025-06-22 22:51:47 +0800

    create hero

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
+import NavBar from './components/NavBar'
+import Hero from './components/hero'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
-  return <div className='flex-center h-[100vh]'>App</div>
+  return (
+    <main className='h-[100vh]'>
+      <NavBar />
+      <Hero />
+    </main>
+  )
 }
 
 export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { navLinks } from '../../constant'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+const NavBar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'nav',
+        start: 'bottom top',
+      },
+    })
+    navTween.fromTo(
+      'nav',
+      { backgroundColor: 'transparent' },
+      {
+        backgroundColor: '#00000050',
+        backgroundFilter: 'blur(10px)',
+        duration: 1,
+        ease: 'power1.inOut',
+      },
+    )
+  })
+  return (
+    <nav className=''>
+      <div>
+        <a href='#home' className='flex items-center gap-2'>
+          <img src='/images/logo.png' alt='logo' />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul className='flex text-white'>
+          {navLinks.map(({ title, id }) => (
+            <li key={id}>
+              <a href={`#${id}`}>{title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
